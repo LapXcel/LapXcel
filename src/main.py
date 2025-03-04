fff76b8e17315e4068c28101c77db6276e66012b
engy-els <91165562+engy-els@users.noreply.github.com>
2025-03-04 17:15:36 -0500

    Broken commit

--- src/main.py
+++ src/main.py
@@ -1,32 +1,50 @@
-from ac_controller import ACController
+import os
+
+import numpy as np
 from ac_socket import ACSocket
+from gymnasium.wrappers import TimeLimit
+from crossq.environment import Env
+from crossq.utils.logx import colorize
+from crossq.crossq import SAC
+
 
 def main():
     """
-    Main Function
+    The main function of the standalone application.
+    It will initialize the environment and the agent, and then run the training loop.
     """
-    controller = ACController()
+    # Car data (Ferrari SF70H)
+    max_speed = 320.0
+
+    # Initialize the environment, max_episode_steps is the maximum amount of steps before the episode is truncated
+    env = TimeLimit(Env(max_speed=max_speed), max_episode_steps=1000)
+
+    # Initialize the agent
+    args = {
+        "adam_b1": 0.5,
+        "policy_delay": 3,
+        "n_critics": 2,
+        "utd": 1,                    # nice
+        "net_arch": {"qf": [2048, 2048]},   # wider critics
+        "bn": True,                # use batch norm
+        "bn_momentum": 0.99,
+        "crossq_style": True,        # with a joint forward pass
+        "tau": 1.0,                  # without target networks
+        "group": f'CrossQ_{args.env}'
+    }
+
+    agent = SAC(env, exp_name, load_path, **hyperparams)
+
+    # Establish a socket connection
     sock = ACSocket()
-
     with sock.connect() as conn:
-        while(1):
-            sock.update()
-            try:
-                # Convert the byte data to a string
-                data_str = sock.data.decode('utf-8')
 
-                # Split the string by commas and map values to a dictionary
-                data_dict = dict(map(lambda x: x.split(':'), data_str.split(',')))
-                print(data_dict)
-            except:
-                # If the data is invalid, throw error and return empty dict
-                print(("Error parsing data, returning empty dict!", "red"))
-                return {}
-            try:
-                controller.perform(0.5, 0)
-                print("Controller input sent")
-            except Exception as e:
-                print(f"Error sending input: {e}")
+        # Set the socket in the environment
+        env.unwrapped.set_sock(sock)
+
+        # Run the training loop
+        agent.train()
+
 
 if __name__ == "__main__":
     """
